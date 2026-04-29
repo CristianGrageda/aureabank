62df5a1547db8e7ce652859dd9c8c5d3191e174a
Cristian Grageda <gragedacristian@gmail.com>
2026-04-29 20:14:01 -0300

    init commit

---
+++ src/main/java/com/gr/aureabank/AureabankApplication.java
@@ -0,0 +1,13 @@
+package com.gr.aureabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AureabankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gr/aureabank/AureabankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr.aureabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AureabankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
